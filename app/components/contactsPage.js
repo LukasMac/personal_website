a6d055aac4f78888fbf148803748a86c981b98f2
Lukas Maciulis <lukasmaciulis@Lukass-MacBook-Pro.local>
2015-10-30 12:53:21 +0100

    Add font awesome icons to contacts page

--- app/components/contactsPage.js
+++ app/components/contactsPage.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Row, Col, Image } from 'react-bootstrap';
+import Icon from 'react-fa';
+
 class ContactsPage extends React.Component {
 
   render() {
     return (
-      <div className="inlineblock">
-        <Row>
-        <Col xs={6} md={4} className="text-right">
-          <h1>Kontaktai</h1>
-        </Col>
-        <Col xs={6} md={8}>
-          One: aaaa
-        </Col>
-        </Row>
+      <div className="inlineblock text-center">
+        <Icon name="github" size="5x" className="social github-container"/>
+        <Icon name="linkedin" size="5x" className="social linkedin-container"/>
+        <Icon name="twitter" size="5x" className="social twitter-container" />
+        <Icon name="envelope-o" size="5x" className="social email-container"/>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,15 @@ var config = {
         test: /\.scss$/,
         loaders: ["style", "css", "sass"]
       },
-      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+
+{
+    test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: "url-loader?limit=10000&minetype=application/font-woff"
+  },{
+    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: "file-loader"
+  }
     ]
   },
 
